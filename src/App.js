176a284d4b9a69c781fba8cf4409f3daaebb8c19
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-02-09 23:22:26 +0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
 import './App.css';
-import {useEffect,useState,useCallback} from 'react';
-
-const API_KEY = 'befb2f4fa3b74919a35105844220902';
-
-async function getWeatherInfo(city){
-  const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
-  const response = await fetch(url);
-  if(!response.ok){
-    throw new Error('Что-то пошло не так');
-  }
-  return await response.json();
-}
+import {useEffect,useState} from 'react';
+import {getWeatherInfo} from './service/WeatherAPIservice';
 
 function WeatherInfoBlock({city}){
-  const [isNotValidResponse,setIsNotValidResponse] = useState(false);
-  const [info,setInfo] =useState({});
+  const [isNotValidResponse, setIsNotValidResponse] = useState(false);
+  const [{temp, flTemp},setInfo] =useState({});
 
-  useEffect(()=>getWeatherInfo(city)
-  .then(data=>{
-      console.log(data.current);
-      setInfo({temp:data.current.temp_c,flTemp:data.current.feelslike_c})
-      setIsNotValidResponse(false);})
-  .catch(()=>{
+  useEffect(() => getWeatherInfo(city)
+  .then(({current}) => {
+      setInfo({temp:current.temp_c, flTemp:current.feelslike_c})
+      setIsNotValidResponse(false); })
+  .catch(() => {
     setIsNotValidResponse(true);
-  }),[city]);
+  }), [city]);
 
   return(
     <div>{isNotValidResponse ? <p>Что-то пошло не так</p>:
-      <p>Температура:{`${info.temp}`} Ощущается как:{`${info.flTemp}`} </p>}
+      <p>Температура:{`${temp}`} Ощущается как:{`${flTemp}`} </p>}
     </div>
   )
 }
 
 function App() {
   const [city,setCity] = useState('');
-  const inputChangeHandler = e =>{
-    setCity(e.target.value);
-  }
-
   return (
     <div className="App">
-        <input type='text' 
-            onChange={inputChangeHandler} />
-        {city ? <WeatherInfoBlock city = {city}/>: <p>Введите город</p>}
+        <input value = {city}
+            type='text' 
+            onChange={(e) => setCity(e.target.value)} />
+        {city ? <WeatherInfoBlock city = {city}/> : <p>Введите город</p>}
     </div>
   );
 }
